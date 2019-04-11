e7ae1e7edf67b7498b9ec67c88ece73926c9e832
Golden Dragon <arajchwald@gmail.com>
2019-04-11 13:32:55 -0500

    sam!

---
+++ app/public/js/search.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    var fighterList = [];
+
+    function displayFighters() {
+        var fightTest = {
+            name: "Dark Sam",
+            hp: 100,
+            atk: 20,
+            speed: 40,
+            armor: 5,
+            spAtk: 15,
+            resistance: 5
+        }
+        $.post("/api/fighters", fightTest)
+    }
+    displayFighters()
+
+})
