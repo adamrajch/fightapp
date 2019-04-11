663fe00ad5256286807d14c460c35eeae0062e66
Golden Dragon <arajchwald@gmail.com>
2019-04-11 11:17:54 -0500

    yee

---
+++ models/fighter.js
@@ -0,0 +1,15 @@
+module.exports = function (sequelize, DataTypes) {
+    var Fighter = sequelize.define("Fighter", {
+
+        name: { type: DataTypes.STRING, allowNull: false },
+        hp: { type: DataTypes.INTEGER, allowNull: false },
+        atk: { type: DataTypes.INTEGER, allowNull: false },
+        speed: { type: DataTypes.INTEGER, allowNull: false },
+        armor: { type: DataTypes.INTEGER, allowNull: false },
+        spAtk: { type: DataTypes.INTEGER, allowNull: false },
+        resistance: { type: DataTypes.INTEGER, allowNull: false },
+        photo: { type: DataTypes.STRING, defaultValue: "../public/images/default.jpg" },
+
+    });
+    return Fighter;
+};
