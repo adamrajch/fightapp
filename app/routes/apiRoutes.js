3840a9698fa460c15d44dd1accdd645b608d3b82
SNORISS <samuel.norris@marquette.edu>
2019-04-11 10:49:10 -0500

    html routes

---
+++ app/routes/apiRoutes.js
@@ -0,0 +1,5 @@
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {};
---
+++ app/routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/profile", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/profile.html"));
+  });
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+  app.get("/search", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/search.html"));
+  });
+  app.get("/fight", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/fight.html"));
+  });
+};
