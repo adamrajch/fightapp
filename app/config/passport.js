7baf98d2e09123ab90edda04b7ba9a7ebe969dec
SNORISS <samuel.norris@marquette.edu>
2019-04-15 10:06:10 -0500

    passport

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,18 +1,105 @@
-var LocalStrategy = require('passport-local').Strategy;
+var bCrypt = require("bcrypt-nodejs");
 
-var db  = require('../models');
+module.exports = function(passport, user) {
+  //LOCAL SIGNIN
+  passport.use(
+    "local-signin",
+    new LocalStrategy(
+      {
+        // by default, local strategy uses username and password, we will override with email
 
-module.exports = function(passport) {
-    passport.serializeUser(function(user, done) {
-        done(null, user.uuid);
-    });
+        usernameField: "username",
 
-    passport.deserializeUser(function(uuid, done) {
-        db.Accounts.findById(uuid).then(function(user) {
-	        if (user) {
-	            done(null, user.get());
-	        } else {
-	            done(user.errors, null);
-	        }
-	    });
-    });
+        passwordField: "password",
+
+        passReqToCallback: true // allows us to pass back the entire request to the callback
+      },
+
+      function(req, username, password, done) {
+        var User = user;
+
+        var isValidPassword = function(userpass, password) {
+          return bCrypt.compareSync(password, userpass);
+        };
+
+        User.findOne({
+          where: {
+            username: username
+          }
+        })
+          .then(function(user) {
+            if (!user) {
+              return done(null, false, {
+                message: "Username does not exist"
+              });
+            }
+
+            if (!isValidPassword(user.password, password)) {
+              return done(null, false, {
+                message: "Incorrect password."
+              });
+            }
+
+            var userinfo = user.get();
+            return done(null, userinfo);
+          })
+          .catch(function(err) {
+            console.log("Error:", err);
+
+            return done(null, false, {
+              message: "Something went wrong with your Signin"
+            });
+          });
+      }
+    )
+  );
+
+  passport.use(
+    "local-signup",
+    new LocalStrategy(
+      {
+        usernameField: "username",
+
+        passwordField: "password",
+
+        passReqToCallback: true // allows us to pass back the entire request to the callback
+      },
+
+      function(req, username, password, done) {
+        var generateHash = function(password) {
+          return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
+        };
+
+        User.findOne({
+          where: {
+            username: username
+          }
+        }).then(function(user) {
+          if (user) {
+            return done(null, false, {
+              message: "That email is already taken"
+            });
+          } else {
+            var userPassword = generateHash(password);
+
+            var data = {
+              username: username,
+
+              password: userPassword
+            };
+
+            User.create(data).then(function(newUser, created) {
+              if (!newUser) {
+                return done(null, false);
+              }
+
+              if (newUser) {
+                return done(null, newUser);
+              }
+            });
+          }
+        });
+      }
+    )
+  );
+};
--- app/models/user.js
+++ app/models/user.js
@@ -2,18 +2,18 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     username: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false }
-    // last_login: { type: sequelize.DATE },
-    // status: {
-    //   type: sequelize.ENUM("active", "inactive"),
-    //   defaultValue: "active"
-    // }
+    last_login: { type: DataTypes.DATE },
+    status: {
+      type: DataTypes.ENUM("active", "inactive"),
+      defaultValue: "active"
+    }
   });
-  //
-  //   User.associate = function(models) {
-  // User.hasOne(models.Fighter, {
-  //   onDelete: "cascade"
-  // });
-  //   };
-  //
+  
+    User.associate = function(models) {
+  User.hasOne(models.Fighter, {
+    onDelete: "cascade"
+  });
+    };
+  
   return User;
 };
