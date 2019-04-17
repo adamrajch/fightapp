358ba694d5ebc47ccd7d967f09d5e47ea2ab98d4
SNORISS <samuel.norris@marquette.edu>
2019-04-16 19:45:40 -0500

    signin and sign up fully working

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -3,5 +3,33 @@ $(document).ready(function() {
   $.get("/api/profile", function(data) {
     console.log("here");
     console.log(data);
+    createFighterCard(data);
   });
 });
+
+function createFighterCard(b) {
+  var fighter = b;
+  var upName = b.class;
+  upName = upName.toUpperCase();
+  var card = `<div class="card classCard" style="width: 18rem;">
+  <img src="${
+    fighter.photo
+  }" class="card-img-top" style="height: 18rem;" alt="your fighter">
+  <div class="card-body">
+  <h1 class="username">${fighter.name}</h1>
+      <h5 class="card-title">${upName} Class Lvl.1</h5>
+      <p class="card-text">Stats</p>
+      <ul>
+      <li>HP: ${fighter.hp} </li>
+      <li>Attack: ${fighter.atk} </li>
+      <li>Sp Atttack: ${fighter.spAtk} </li>
+      <li>Speed: ${fighter.speed} </li>
+      <li>Armor: ${fighter.armor} </li>
+      <li>Resistance: ${fighter.resistance} </li>
+      <li>Win: ${fighter.win} </li>
+      <li>Losses: ${fighter.loss} </li>
+      </ul>
+  </div>
+</div>`;
+  $(".display").append(card);
+}
