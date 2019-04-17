20b73d681b693b7b66811fc67182efdadc21d94b
Golden Dragon <arajchwald@gmail.com>
2019-04-17 06:24:51 -0500

    YEET FINAL PUSH

--- app/public/js/fight.js
+++ app/public/js/fight.js
@@ -1,37 +1,8 @@
-<<<<<<< HEAD
-import { ENOMEM } from "constants";
-
+// import { getEnabledCategories } from "trace_events";
+// import { ENOMEM } from "constants";
 $(document).ready(function () {
 
-    console.log("hello fight")
-    let player;
-    let enemy;
-    let playerFirst;
-    let eHp;
-    let pHp;
-    let atks;
-    let cpuAtks;
-    let logArr = [];
-
-    $.get('/api/fighters', function (data, err) {
-        var p1 = data[0];
-        player = data[0];
-        enemy = data[1];
-        console.log(p1);
-        console.log(data[1]);
-        displayPlayer(p1)
-        displayEnemy(data[1])
-        eHp = data[1].hp;
-        pHp = data[0].hp
-        speedCalc()
-    })
-    function displayPlayer(a) {
-        var p1 = a;
-        var card = `<div class="card classCard" style="width: 18rem;">
-      <img src="${p1.photo}" class="card-img-top" style="height: 18rem;" alt="your p1">
-=======
-$(document).ready(function() {
-  console.log("hello fight");
+  console.log("hello fight")
   let player;
   let enemy;
   let playerFirst;
@@ -39,29 +10,51 @@ $(document).ready(function() {
   let pHp;
   let atks;
   let cpuAtks;
-
-  $.get("/api/profile", function(data, err) {
-    var p1 = data;
+  let logArr = [];
+  var done = false;
+  let playerLose;
+  var yeet = true;
+  $.get("/api/profile", function (data) {
+    console.log("here");
+    console.log(data);
     player = data;
-    $.get("/api/fighters/id/" + data.enemy, function(data) {
-      enemy = data[0];
-      console.log(data[0]);
-      //console.log(p1);
-      //console.log(data[1]);
-      displayPlayer(p1);
-      displayEnemy(enemy);
-      eHp = enemy.hp;
-      pHp = player.hp;
-      speedCalc();
-    });
+    displayPlayer(player);
+
+    getEnemy(player);
+
   });
+
+  function getEnemy(p) {
+    $.get("/api/fighters/id/" + p.enemy, function (data) {
+      console.log(data, data[0])
+      enemy = data[0];
+      displayEnemy(enemy);
+      speedCalc()
+      pHp = player.hp;
+      eHp = enemy.hp;
+    })
+
+  }
+
+
+
+  // $.get('/api/fighters', function (data, err) {
+  //   var p1 = data[0];
+  //   player = data[0];
+  //   enemy = data[1];
+  //   console.log(p1);
+  //   console.log(data[1]);
+  //   displayPlayer(p1)
+  //   displayEnemy(data[1])
+  //   eHp = data[1].hp;
+  //   pHp = data[0].hp
+
+  //   speedCalc()
+  // })
   function displayPlayer(a) {
     var p1 = a;
     var card = `<div class="card classCard" style="width: 18rem;">
-      <img src="${
-        p1.photo
-      }" class="card-img-top" style="height: 18rem;" alt="your p1">
->>>>>>> master
+      <img src="${p1.photo}" class="card-img-top" style="height: 18rem;" alt="your p1">
       <div class="card-body">
       <div class="progress">
       <div class="progress-bar progress-bar-striped progress-bar-animated playerHP" role="progressbar"
@@ -86,10 +79,10 @@ $(document).ready(function() {
   }
 
   function displayEnemy(a) {
+    console.log("display enemy working")
+    var upper = a.class.toUpperCase()
     var card = `<div class="card classCard" style="width: 18rem;">
-        <img src="${
-          a.photo
-        }" class="card-img-top" style="height: 18rem;" alt="your a">
+        <img src="${a.photo}" class="card-img-top" style="height: 18rem;" alt="your a">
         <div class="card-body">
         <div class="progress">
         <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger enemyHP"
@@ -97,7 +90,7 @@ $(document).ready(function() {
             style="width: 100%"></div>
     </div>
         <h1 class="username">${a.name}</h1>
-            <h5 class="card-title">${a.class.toUpperCase()} Class Lvl.1</h5>
+            <h5 class="card-title">${upper} Class Lvl.1</h5>
             <p class="card-text">Stats</p>
             <ul>
             <li>HP: ${a.hp} </li>
@@ -114,6 +107,7 @@ $(document).ready(function() {
     $(".enemyCol").append(card);
   }
   function speedCalc() {
+    console.log("speed calc called")
     pSd = player.speed;
     eSd = enemy.speed;
 
@@ -144,6 +138,7 @@ $(document).ready(function() {
     }
   }
   function damageDealtAtk() {
+    appendHR()
     // var rndAtk = Math.floor(Math.random() * (player.atk + 2) + (player.atk - 2))
     // var dmg = rndAtk - enemy.armor;
     // eHp -= rndAtk - enemy.armor;
@@ -182,74 +177,10 @@ $(document).ready(function() {
         enemyAction();
       }
     }
-<<<<<<< HEAD
-    function playerAtk() {
-        if (player.hp <= 0) {
-            console.log("u died bro")
-        }
-        else {
-            var rndAtk = Math.floor(Math.random() * (player.atk + 2) + (player.atk - 2))
-            var dmg = rndAtk - enemy.armor;
-            eHp -= rndAtk - enemy.armor;
-
-            if (eHp <= 0) {
-                var pLog = `<li class="list-group-item list-group-item-primary">${player.name} attacked for ${dmg} damage! ${enemy.name} has ${eHp} left!</li>`;
-                $(".log").prepend("<hr>");
-                $(".log").prepend(pLog);
-                var winLog = `<li class="list-group-item list-group-item-primary">${enemy.name} is defated! ${player.name} wins!</li>`;
-                $(".pAttack").hide()
-                $(".spAttack").hide()
-                $(".log").prepend(winLog);
-
-            }
-
-            else {
-                var pLog = `<li class="list-group-item list-group-item-primary">${player.name} attacked for ${dmg} damage! ${enemy.name} has ${eHp} left!</li>`;
-                $(".log").prepend("<hr>");
-                $(".log").prepend(pLog);
-            }
-        }
-
-
-
-    }
-    function enemyAtk() {
-        if (enemy.hp <= 0) {
-            console.log("enemy died")
-        }
-        else {
-            var enemyrndAtk = Math.floor(Math.random() * (enemy.atk + 2) + (enemy.atk - 2))
-            var edmg = enemyrndAtk - player.armor;
-            pHp -= edmg;
-            var eLog = `<li class="list-group-item list-group-item-danger">${enemy.name} attacked for ${edmg} damage! ${player.name} has ${pHp} left!</li>`;
-            // logArr.push(eLog);
-            // $(".log").prepend(eLog);
-
-            // console.log($(".log"))
-            if (pHp <= 0) {
-                // var eLog = `<li class="list-group-item list-group-item-primary">${enemy.name} attacked for ${edmg} damage! ${player.name} has ${pHp} left!</li>`;
-                $(".log").prepend("<hr>");
-                $(".log").prepend(eLog);
-                var winLog = `<li class="list-group-item list-group-item-primary">${player.name} is defated! ${enemy.name} wins!</li>`;
-                $(".pAttack").hide()
-                $(".spAttack").hide()
-                $(".log").prepend(winLog);
-
-            }
-
-            else {
-                $(".log").prepend("<hr>");
-                $(".log").prepend(eLog);
-            }
-        }
-
-    }
-
-    // function damageDealtSpAtk(spattack, hp, res) {
-=======
   }
 
   function damageDealtSpAtk() {
+    appendHR();
     // var rndAtk = Math.floor(Math.random() * (player.atk + 2) + (player.atk - 2))
     // var dmg = rndAtk - enemy.armor;
     // eHp -= rndAtk - enemy.armor;
@@ -263,26 +194,33 @@ $(document).ready(function() {
 
     if (playerFirst) {
       if (atks == 1 && cpuAtks == 1) {
+        // appendHR();
         playerSpAtk();
         enemyAction();
+
       } else if (atks == 2) {
+        // appendHR();
         playerSpAtk();
         enemyAction();
         playerSpAtk();
       } else if (cpuAtks == 2) {
+        // appendHR();
         playerSpAtk();
         enemyAction();
         enemyAction();
       }
     } else {
       if (atks == 1 && cpuAtks == 1) {
+        // appendHR();
         enemyAction();
         playerSpAtk();
       } else if (atks == 2) {
+        // appendHR();
         enemyAction();
         playerSpAtk();
         playerSpAtk();
       } else if (cpuAtks == 2) {
+        // appendHR();
         enemyAction();
         playerSpAtk();
         enemyAction();
@@ -298,57 +236,145 @@ $(document).ready(function() {
     }
   }
   function playerAtk() {
-    var rndAtk = Math.floor(
-      Math.random() * (player.atk + 2) + (player.atk - 2)
-    );
-    var dmg = rndAtk - enemy.armor;
-    eHp -= rndAtk - enemy.armor;
->>>>>>> master
+    if (yeet) {
 
-    var pLog = `<li class="list-group-item list-group-item-primary">${
-      player.name
-    } attacked for ${dmg} damage! ${enemy.name} has ${eHp} left!</li>`;
-    $(".log").prepend(pLog);
+      var rndAtk = Math.floor(
+        Math.random() * (player.atk + 2) + (player.atk - 2)
+      );
+      var dmg = rndAtk - enemy.armor;
+      eHp -= rndAtk - enemy.armor;
+      var newHP = eHp;
+
+      var chb = (eHp / enemy.hp) * 100;
+      console.log(chb)
+      var newHealth = Math.ceil(chb);
+      // var chb = ;
+      $(".enemyHP").css("width", `${newHealth}%`);
+
+      // changeEHB(newHP);
+
+
+
+      if (eHp <= 0) {
+        var pLog = `<li class="list-group-item list-group-item-primary pa">${
+          player.name
+          } attacked for ${dmg} damage! ${enemy.name} has ${eHp} left! ${enemy.name} has been defeated!</li> `;
+        $(".log").prepend(pLog);
+        playerLose = false;
+        yeet = false;
+        endGame(playerLose)
+        $(".enemyHP").css("width", `0`);
+      }
+      else {
+        var pLog = `<li class="list-group-item list-group-item-primary pa">${
+          player.name
+          } attacked for ${dmg} damage! ${enemy.name} has ${eHp} left!</li>`;
+        $(".log").prepend(pLog);
+      }
+
+    }
   }
+  // function changeEHB(b) {
+  //   var chb = Math.ceil(b / enemy.hp);
+  //   $(".enemyHP").css("width", `${chb}`);
+
+  // }
   function playerSpAtk() {
-    var rndAtk = Math.floor(
-      Math.random() * (player.spAtk + 2) + (player.spAtk - 2)
-    );
-    var dmg = Math.floor(rndAtk * ((100 - enemy.resistance) / 100));
-    eHp -= dmg;
+    if (yeet) {
+      var rndAtk = Math.floor(
+        Math.random() * (player.spAtk + 2) + (player.spAtk - 2)
+      );
+      var dmg = Math.floor(rndAtk * ((100 - enemy.resistance) / 100));
+      eHp -= dmg;
+      var chb = (eHp / enemy.hp) * 100;
+      console.log(chb)
+      var newHealth = Math.ceil(chb);
+      // var chb = ;
+      $(".enemyHP").css("width", `${newHealth}%`);
+      if (eHp <= 0) {
+        var pLog = `<li class="list-group-item list-group-item-primary pa">${
+          player.name
+          } casted a spell for ${dmg} damage! ${enemy.name} has ${eHp} left! ${enemy.name} has been defeated!</li> `;
+        $(".log").prepend(pLog);
+        playerLose = false;
+        yeet = false;
+        endGame(playerLose)
+        $(".enemyHP").css("width", `0`);
+      }
+      else {
+        var pLog = `<li class="list-group-item list-group-item-primary pa ">${
+          player.name
+          } casted a spell for ${dmg} damage! ${enemy.name} has ${eHp} left!</li>`;
+        $(".log").prepend(pLog);
+      }
 
-    var pLog = `<li class="list-group-item list-group-item-primary">${
-      player.name
-    } casted a spell for ${dmg} damage! ${enemy.name} has ${eHp} left!</li>`;
-    $(".log").prepend(pLog);
+    }
   }
+
   function enemyAtk() {
-    var enemyrndAtk = Math.floor(
-      Math.random() * (enemy.atk + 2) + (enemy.atk - 2)
-    );
-    var edmg = enemyrndAtk - player.armor;
-    pHp -= edmg;
-    var eLog = `<li class="list-group-item list-group-item-danger">${
-      enemy.name
-    } attacked for ${edmg} damage! ${player.name} has ${pHp} left!</li>`;
-    $(".log").prepend(eLog);
+    if (yeet) {
+      var enemyrndAtk = Math.floor(
+        Math.random() * (enemy.atk + 2) + (enemy.atk - 2)
+      );
+      var edmg = enemyrndAtk - player.armor;
+      pHp -= edmg;
+      var chb = (pHp / player.hp) * 100;
+      console.log(chb)
+      var newHealth = Math.ceil(chb);
+      // var chb = ;
+      $(".playerHP").css("width", `${newHealth}%`);
+
+      if (pHp <= 0) {
+        var eLog = `<li class="list-group-item list-group-item-danger ea">${
+          enemy.name
+          } attacked for ${edmg} damage! ${player.name} has ${pHp} left! ${player.name} has been defeated!</li>`;
+        $(".log").prepend(eLog);
+        playerLose = true;
+        endGame(playerLose);
+        yeet = false;
+        $(".playerHP").css("width", `0`);
+      }
+      else {
+        var eLog = `<li class="list-group-item list-group-item-danger ea">${
+          enemy.name
+          } attacked for ${edmg} damage! ${player.name} has ${pHp} left!</li>`;
+        $(".log").prepend(eLog);
+      }
+    }
   }
 
   function enemySpAtk() {
-    var enemyrndAtk = Math.floor(
-      Math.random() * (enemy.spAtk + 2) + (enemy.spAtk - 2)
-    );
-    var edmg = Math.floor(enemyrndAtk * ((100 - player.resistance) / 100));
-    pHp -= edmg;
-    var eLog = `<li class="list-group-item list-group-item-danger">${
-      enemy.name
-    } casted a spell for ${edmg} damage! ${player.name} has ${pHp} left!</li>`;
-    $(".log").prepend(eLog);
+    if (yeet) {
+
+      var enemyrndAtk = Math.floor(
+        Math.random() * (enemy.spAtk + 2) + (enemy.spAtk - 2)
+      );
+      var edmg = Math.floor(enemyrndAtk * ((100 - player.resistance) / 100));
+      pHp -= edmg;
+      var chb = (pHp / player.hp) * 100;
+      console.log(chb)
+      var newHealth = Math.ceil(chb);
+      // var chb = ;
+      $(".playerHP").css("width", `${newHealth}%`);
+      if (pHp <= 0) {
+        var eLog = `<li class="list-group-item list-group-item-danger ea">${
+          enemy.name
+          } casted a spell for ${edmg} damage! ${player.name} has ${pHp} left! ${player.name} has been defeated!</li>`;
+        $(".log").prepend(eLog);
+        playerLose = true;
+        endGame(playerLose);
+      }
+
+      var eLog = `<li class="list-group-item list-group-item-danger ea">${
+        enemy.name
+        } casted a spell for ${edmg} damage! ${player.name} has ${pHp} left!</li>`;
+      $(".log").prepend(eLog);
+    }
   }
   // function damageDealtSpAtk(spattack, hp, res) {
 
   // }
-  $(document).on("click", ".beginFight", function() {
+  $(document).on("click", ".beginFight", function () {
     fight();
   });
 
@@ -358,12 +384,54 @@ $(document).ready(function() {
     $(".battleLog").prepend(butts);
     $(".beginFight").hide();
   }
-  $(document).on("click", ".pAttack", function() {
-    // speedCalc();
-
+  $(document).on("click", ".pAttack", function () {
     damageDealtAtk();
   });
-  $(document).on("click", ".spAttack", function() {
+  $(document).on("click", ".spAttack", function () {
     damageDealtSpAtk();
   });
+
+  function endGame(b) {
+    if (b) {
+      $(".pAttack").hide()
+      $(".spAttack").hide()
+      $(".logContainer").css("opacity", "0.5")
+      $(".battleLog").prepend("YOU LOSE")
+      enemy.win++;
+      player.loss++;
+
+      playerResult();
+      enemyResult();
+    }
+    else {
+      $(".pAttack").hide()
+      $(".spAttack").hide()
+      // $(".log").empty();
+      $(".logContainer").css("opacity", "0.5")
+      $(".battleLog").prepend("YOU WIN")
+      enemy.loss++;
+      player.win++;
+      playerResult()
+      enemyResult()
+    }
+  }
+  function appendHR() {
+    $(".log").prepend("<hr>")
+  }
+  function playerResult() {
+
+    $.ajax({
+      url: "/api/update",
+      type: "PUT",
+      data: player
+    });
+  }
+  function enemyResult() {
+
+    $.ajax({
+      url: "/api/update",
+      type: "PUT",
+      data: enemy
+    });
+  }
 });
